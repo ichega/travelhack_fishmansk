bbf334b3c4cf71f3775884db23ee21c6698a2b23
kamych1916 <kamych1916@gmail.com>
2020-02-01 16:35:33 +0300

    add rest logic in all components

--- frontend/src/app/stem/stem.component.ts
+++ frontend/src/app/stem/stem.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './interface';
+import { HttpService } from './http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stem',
   templateUrl: './stem.component.html',
-  styleUrls: ['./stem.component.css']
+  styleUrls: ['./stem.component.css'],
+  providers: [HttpService],
 })
-export class StemComponent implements OnInit {
+export class StemComponent {
   stem="none"
-  constructor() { }
+  user: User = new User(); // данные от пользователя
+      
+  constructor(private httpService: HttpService, public router: Router){}
 
-  ngOnInit() {
-  }
+  receivedData: User; // полученные данные
+
+  
+  sendDataUser(user: User){
+    if(user.Email == undefined){alert('email input value is empty!')}
+    else{
+    this.httpService.postData(user)
+      .subscribe(
+        (data: User) => {this.receivedData=data, alert('we send new password in your mail adress!'); this.router.navigate(['/auth'])},
+        error => { if(error.status == 500){ alert('invalid login')} else {alert('invalid password') }}
+      );
+    }
+  };
 
 }
