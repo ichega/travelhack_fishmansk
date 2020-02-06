6ea4c5cc583dc02465532cbdf79abe877b1eea59
kamych1916 <kamych1916@gmail.com>
2020-02-06 16:58:52 +0300

    add modal

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -6,9 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  openModal: boolean=false;
+  
+  isLoadingOne = false;
+
+
+
   onBack(): void {
     console.log('onBack');
   }
+
+  ModalEvent(type){
+    this.isLoadingOne = true;
+    if(type == 1){
+      setTimeout(() => {
+        this.isLoadingOne = false;
+        this.openModal = true
+      }, 2000);
+    }else{
+      this.openModal = false
+      this.isLoadingOne = false;
+    }
+
+  }
+
   constructor() { }
 
   ngOnInit() {
