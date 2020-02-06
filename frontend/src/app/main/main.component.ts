af068248509e692dd78a7c1d860c736ca1496cae
kamych1916 <kamych1916@gmail.com>
2020-02-06 15:02:50 +0300

    add header block

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  onBack(): void {
+    console.log('onBack');
+  }
   constructor() { }
 
   ngOnInit() {
